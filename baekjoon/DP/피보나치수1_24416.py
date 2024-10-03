0af4960de5235ff715c2ba3cfaa8acdd7662daec
HugeKyung <kiki9510@gmail.com>
2024-10-03 23:17:09 +0900

    chore: 피보나치수1 수정사항 반영

--- baekjoon/DP/피보나치수1_24416.py
+++ baekjoon/DP/피보나치수1_24416.py
@@ -5,3 +5,12 @@ def fib(n):
     return fib(n - 1) + fib(n - 2)
 
 print(fib(int(input())))
+
+# DP 문제풀이
+def fib_dp(n):
+    a, b = 1, 1
+    for _ in range(3, n+1):
+        a, b = b, a + b # 값 스왑을 통해 직전 값 기억(직전 b값 -> a값)
+    
+    return b
+
