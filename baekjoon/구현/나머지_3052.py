159bb5ddc07799e87b912c3aad496fae796c0493
HugeKyung <kiki9510@gmail.com>
2024-10-14 01:16:44 +0900

    나머지 3052 헤이밥 풀이 추가

--- baekjoon/구현/나머지_3052.py
+++ baekjoon/구현/나머지_3052.py
@@ -7,3 +7,14 @@ for _ in range(10):
     unique_set.add(na)
 
 print(len(unique_set))
+
+# 헤이밥 풀이
+# 모듈 1: 10개의 숫자 배열 -> 각 숫자를 42로 나눈 배열 생성
+arr = [int(input()) for _ in range(10)]
+arr_42 = list(map(lambda elem: elem % 42, arr))
+
+# 모듈 2: arr_42 -> 고유한 숫자들만 남긴 배열 생성
+arr_42_unique = list(set(arr_42))
+
+# 모듈 3: arr_42_unique -> 해당 배열의 개수 출력
+print(len(arr_42_unique))
