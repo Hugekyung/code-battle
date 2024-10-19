270bf49e72cc3f6cf0debc133e64424b5aa3eaa4
HugeKyung <kiki9510@gmail.com>
2024-10-19 14:45:53 +0900

    feat: 색종이 자르기 2630번 풀이 추가

---
+++ baekjoon/재귀/색종이자르기_2630.py
@@ -0,0 +1,32 @@
+# 색종이자르기 2630번
+# 2024-10-19
+# 종이의 색이 모두 같은 색이 아닐 때 -> 똑같은 크기의 4개 색종이로 자른다
+# 자른 종이의 색이 모두 같은 색이 될 때까지 반복(재귀)
+# 종료조건: 종이의 색이 모두 같은 색일 때(1 or 0)
+
+n = int(input())
+paper = [list(map(int, input().split())) for _ in range(n)]
+paper_cnt = { 0: 0, 1: 0 }
+def cut_paper(x, y, n, paper_cnt):
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if paper[i][j] == paper[x][y]: # 0,0 위치의 색과 같은지 확인
+                continue
+            else:
+                # 색이 다름 -> 색종이 자르기
+                cut_paper(x, y, n//2, paper_cnt) # 1사분면
+                cut_paper(x, y + n//2, n//2, paper_cnt) # 2사분면
+                cut_paper(x + n//2, y, n//2, paper_cnt) # 3사분면
+                cut_paper(x + n//2, y + n//2, n//2, paper_cnt) # 4사분면
+                return
+    
+    # 종이의 색이 모두 같음
+    if paper[x][y] == 1: # 파란색
+        paper_cnt[1] += 1
+    else: # 하얀색
+        paper_cnt[0] += 1
+    return
+
+cut_paper(0, 0, n, paper_cnt)
+print(paper_cnt[0])
+print(paper_cnt[1])
