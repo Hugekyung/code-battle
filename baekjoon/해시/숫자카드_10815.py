8596e4872c00d1b74c2b38157e5efdc45b70cf0a
HugeKyung <kiki9510@gmail.com>
2024-10-26 20:02:14 +0900

    feat: 숫자카드 2회차 문제풀이 추가

--- baekjoon/해시/숫자카드_10815.py
+++ baekjoon/해시/숫자카드_10815.py
@@ -11,3 +11,15 @@ for card in cards:
         print(1, end = " ")
     else:
         print(0, end = " ")
+
+# 숫자 카드 10815번 2회차 풀이
+# 해시
+n = int(input())
+set_my_card = set(list(map(int, input().split())))
+m = int(input())
+cards = list(map(int, input().split()))
+for card in cards:
+    if card in set_my_card:
+        print(1, end=" ")
+    else:
+        print(0, end=" ")
