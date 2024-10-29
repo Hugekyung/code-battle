2ab610d3ddcaae85bfbc5fc0524a5dd18bc07d6e
HugeKyung <kiki9510@gmail.com>
2024-10-29 23:14:47 +0900

    feat: 색종이 만들기 3회차 풀이 추가

--- baekjoon/재귀/색종이만들기_2630.py
+++ baekjoon/재귀/색종이만들기_2630.py
@@ -1,4 +1,4 @@
-# 색종이자르기 2630번
+# 색종이 만들기 2630번
 # 2024-10-19
 # 종이의 색이 모두 같은 색이 아닐 때 -> 똑같은 크기의 4개 색종이로 자른다
 # 자른 종이의 색이 모두 같은 색이 될 때까지 반복(재귀)
@@ -55,3 +55,30 @@ def cut_paper(x, y, n):
 cut_paper(0, 0, n)
 print(result[0])
 print(result[1])
+
+
+# 색종이 만들기 3회차 풀이
+n = int(input())
+arr = [list(map(int, input().split())) for _ in range(n)]
+result = { 0: 0, 1: 0 }
+
+def cut_paper(x, y, n):
+    global result
+    for i in range(x, x+n):
+        for j in range(y, y+n):
+            if arr[x][y] != arr[i][j]: # 시작점의 색과 같은지 확인 -> 다르면 자르기
+                cut_paper(x, y, n//2)
+                cut_paper(x, y + n//2, n//2)
+                cut_paper(x + n//2, y, n//2)
+                cut_paper(x + n//2, y + n//2, n//2)
+                return
+
+    # 여기까지 오면 색이 같은 것들만 모여있다고 확인
+    if arr[x][y]:
+        result[1] += 1
+    else:
+        result[0] += 1
+
+cut_paper(0, 0, n)
+print(result[0]) # 하얀색
+print(result[1]) # 파란색
