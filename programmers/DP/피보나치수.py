fcd740d4aef9228fbec22fd75400f2ec5cf21812
HugeKyung <kiki9510@gmail.com>
2024-11-06 00:55:06 +0900

    feat: 피보나치 수 2회차 풀이 추가

--- programmers/DP/피보나치수.py
+++ programmers/DP/피보나치수.py
@@ -12,3 +12,16 @@ def solution(n):
         dp[i] = (dp[i - 1] + dp[i - 2])
         
     return dp[n] % 1234567
+
+# 2회차 풀이
+# 점화식: fibo(n) = fibo(n-1) + fibo(n-2)
+# fibo(1) = 1
+def solution(n):
+    if n == 1: return 1 % 1234567
+    dp = [0] * (n+1)
+    dp[1] = 1
+    
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+        
+    return dp[n] % 1234567
