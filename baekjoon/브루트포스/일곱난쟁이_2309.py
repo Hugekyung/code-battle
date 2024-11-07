353a7ffbb5790629ee138074ac22385cc106b657
HaeChan Yang <kiki9510@gmail.com>
2024-11-07 13:01:18 +0900

    feat: 일곱난쟁이 2309번 4회차 문제풀이 추가

--- baekjoon/브루트포스/일곱난쟁이_2309.py
+++ baekjoon/브루트포스/일곱난쟁이_2309.py
@@ -1,4 +1,4 @@
-# 일곱 난쟁이 2309번
+###### 일곱 난쟁이 2309번 ######
 # 브루트포스/정렬
 from itertools import permutations
 
@@ -17,3 +17,21 @@ def picker(arr, s): # s: 선택하고자 하는 개수
 res = picker(arr, 7)
 for i in range(len(res)):
     print(res[i], end="\n")
+
+###### 일곱난쟁이 2309번 4회차 풀이 ######
+# 시간 제한 2초 -> 파이썬 기준 1초 2,000만번 -> 총 4,000만번 가능
+import sys
+
+arr = sorted([int(sys.stdin.readline()) for _ in range(9)])
+sum_arr = sum(arr)
+is_break = False
+for i in range(len(arr)):
+    if is_break: break
+    for j in range(i+1, len(arr)):
+        if sum_arr - arr[i] - arr[j] == 100:
+            for k in arr:
+                if k == arr[i] or k == arr[j]:
+                    continue
+                else:
+                    print(k, end="\n")
+            is_break = True
