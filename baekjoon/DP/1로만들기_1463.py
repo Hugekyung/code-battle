c9adc6730b9b4790b767fe31891b569006231666
HugeKyung <kiki9510@gmail.com>
2024-11-29 01:36:40 +0900

    feat: 1로만들기 2024 1회차 풀이 추가

--- baekjoon/DP/1로만들기_1463.py
+++ baekjoon/DP/1로만들기_1463.py
@@ -22,3 +22,18 @@ for i in range(2, x + 1):
         dp[i] = min(dp[i], dp[i // 5] + 1)
 
 print(dp[x])
+
+# 1로 만들기 2024-11-29 풀이
+def make_one(dp, n):
+    for i in range(2, n+1):
+        dp[i] = dp[i-1] + 1 # 1을 빼는 경우
+        if i % 2 == 0:
+            dp[i] = min(dp[i], dp[i//2] + 1)
+        if i % 3 == 0:
+            dp[i] = min(dp[i], dp[i//3] + 1)
+        
+    return dp[n]
+
+n = int(input())
+dp = [0] * (n+1)
+print(make_one(dp, n))
