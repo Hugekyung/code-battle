587d0bc35f5823fda4de5b6860c6188dcaa39f9b
Hugekyung <kiki9510@gmail.com>
2025-09-17 02:10:08 +0900

    feat: missing integer 주석 추가

--- codility/missingInteger.js
+++ codility/missingInteger.js
@@ -1,3 +1,6 @@
+// * 배열에서 존재하지 않는 가장 작은 양의 정수 찾기
+// * 해결 포인트: 중복, 음수, 0, 큰 수 범위 예외처리
+// * 핵심 스킬: Set, filter 기반 positive 원소 추출
 function solution(A) {
     const positiveSet = new Set(A.filter((a) => a > 0));
     for (let i = 1; i <= positiveSet.size + 1; i++) {
